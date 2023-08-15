86d91ff1e5ef61f9733399296d67df4fb504f3bd
Sergey Matveev <serega-matveef@yandex.ru>
2023-08-15 20:13:16 +0300

    добавил react-app и изменил наполнение

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Здесь скоро будет <code>Skypro.Music</code>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
