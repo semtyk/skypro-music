f6478e9c0e5365210347f7e930261767bbc87146
Sergey Matveev <serega-matveef@yandex.ru>
2023-09-04 23:06:50 +0300

    добавил скрытие бургер меню

--- src/components/navmenu.js
+++ src/components/navmenu.js
@@ -1,17 +1,30 @@
 import './navmenu.css';
+import { useState } from 'react';
 
 export default function NavMenu() {
+    const [visible, setVisible] = useState(true);
+    
+    const handleClick = () => {
+      setVisible(!visible);
+    }
+
+    function handleKeyDown(e) {
+      if (e.keyCode === 13) {
+        handleClick();
+      }
+    }
+
     return (
         <nav className="main__nav nav">
           <div className="nav__logo logo">
             <img className="logo__image" src="./img/logo.png" alt="logo" />
           </div>
-          <div className="nav__burger burger">
+          <div role='button' tabIndex={0} className="nav__burger burger" onClick={handleClick} onKeyDown={handleKeyDown}>
             <span className="burger__line" />
             <span className="burger__line" />
             <span className="burger__line" />
           </div>
-          <div className="nav__menu menu">
+          {visible && (<div className="nav__menu menu">
             <ul className="menu__list">
               <li className="menu__item">
                 <a href='/#' className="menu__link">
@@ -29,7 +42,7 @@ export default function NavMenu() {
                 </a>
               </li>
             </ul>
-          </div>
+          </div>)}
         </nav>
     )
 }
