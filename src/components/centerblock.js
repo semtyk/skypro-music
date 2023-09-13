9bebd439d3843e14993673e8c4fa8f2ce8b6fc3c
Sergey Matveev <serega-matveef@yandex.ru>
2023-09-14 00:07:59 +0300

    реализовал функционал переключения фильтров

--- src/components/centerblock.js
+++ src/components/centerblock.js
@@ -1,22 +1,64 @@
+import { useState } from 'react'
 import SearchPannel from './centerblock-search'
 import './centerblock.css'
 import TrackList from './tracklist'
 
+function FilterItem(props) {
+
+
+  return (
+    <div className='filterItem'>
+    <button type='button' className = {`${props.className} filter__button _btn-text ${props.isActive && 'filter__button_active'}`} onClick={props.onShow}>
+        {props.title}
+    </button>
+    {props.isActive && (<div className='filterItem__scrollBar'>
+      <ul className='filterItem__listItems'>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+      </ul>
+    </div>)}
+
+    </div>
+  )
+}
+
 
 export default function CenterBlock(props) {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+    function clbSetActiveIndex(key) {
+      return () => {
+          if (activeIndex === key) {
+            setActiveIndex(0);
+          } else {
+            setActiveIndex(key);
+          }   
+      }
+    }
+
     return (
         <div className="main__centerblock centerblock">
           <SearchPannel />
           <h2 className="centerblock__h2">Треки</h2>
           <div className="centerblock__filter filter">
             <div className="filter__title">Искать по:</div>
-            <div className="filter__button button-author _btn-text">
-              исполнителю
-            </div>
-            <div className="filter__button button-year _btn-text">
-              году выпуска
-            </div>
-            <div className="filter__button button-genre _btn-text">жанру</div>
+            <FilterItem className="button-author" title='исполнителю' onShow={clbSetActiveIndex(1)} isActive={activeIndex===1}/>
+            <FilterItem className="button-year" title='году выпуска' onShow={clbSetActiveIndex(2)} isActive={activeIndex===2}/>
+            <FilterItem className="button-genre" title='жанру' onShow={clbSetActiveIndex(3)} isActive={activeIndex===3}/>
           </div>
           <div className="centerblock__content">
             <div className="content__title playlist-title">
