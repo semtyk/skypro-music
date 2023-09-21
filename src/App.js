79d1aa326e7765770968bffe78948e93a93e6c98
Sergey Matveev <serega-matveef@yandex.ru>
2023-09-22 00:36:12 +0300

    create global style and finish work

--- src/App.js
+++ src/App.js
@@ -1,48 +1,39 @@
-import './App.css';
 import { useState, useEffect } from 'react';
 import AudioPlayer from './components/audioplayer/audioplayer';
 import CenterBlock from './components/centerblock/centerblock';
 import NavMenu from './components/navmenu/navmenu';
 import SideBar from './components/sidebar/sidebar';
-
+import * as S from './components/styledApp/styled.app'
 
 
 function App() {
    const [isSkeleton, setIsSkeleton] = useState(true);
-   // const [isBlink, setIsBlink] = useState(0); 
 
     useEffect(()=> {
-      
-      // const intervalId = setInterval(() => {
-      //   // eslint-disable-next-line no-shadow
-      //   setIsBlink(isBlink => !isBlink);
-      // }, 500);
-
       const timerId = setTimeout(() => {
         setIsSkeleton(!isSkeleton);
-        // clearInterval(intervalId);
       }, 5000);
       
       return () => {
         clearTimeout(timerId);
-        // clearInterval(intervalId);
       };
     }, []);
-  
-
 
   return (
-  <div className="wrapper">
-    <div className="container">
-      <main className="main">
+  <>
+  <S.GlobalStyle/>
+  <S.Wrapper>
+    <S.Container>
+      <S.Main>
         <NavMenu />
         <CenterBlock isSkeleton = {isSkeleton} />
         <SideBar unvisible = {isSkeleton}/>
-      </main>
+      </S.Main>
       <AudioPlayer unvisible = {isSkeleton}/>
-      <footer className="footer" />
-    </div>
-  </div>
+      <footer />
+    </S.Container>
+  </S.Wrapper>
+  </>
   );
 }
 
